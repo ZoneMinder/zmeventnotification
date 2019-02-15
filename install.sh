466a48890a7ec2e0e25f84f9aa938cff52b54bb5
Pliable Pixels <pliablepixels@gmail.com>
2019-02-15 15:50:59 -0500

    install script foo

--- install.sh
+++ install.sh
@@ -3,28 +3,56 @@
 TARGET_BIN='/usr/bin'
 TARGET_ZMES_CONFIG='/etc'
 TARGET_HOOK_CONFIG='/var/detect/config'
-WEB_OWNER='www-data' # change to apache or whomever your distro web user is
+WEB_OWNER=$(ps -ef | grep -E '(httpd|hiawatha|apache|apache2)' | grep -v whoami | grep -v root | head -n1 | awk '{print $1}' )
+#WEB_OWNER='www-data' # change to apache or whomever your distro web user is
 
 # make this empty if you don't want backups
 MAKE_CONFIG_BACKUP='-b'
 
+
+if [[ $EUID -ne 0 ]]
+then
+    echo 
+    echo "---------------------------- ---------------------------------------------------"
+    echo "WARNING: Unless you have changed paths, this script requires to be run as sudo"
+    echo "--------------------------------------------------------------------------------"
+    echo
+    read -p "Press any key to continue or ^C to quit and run again with sudo..."
+
+fi
+
+echo "Detected web group as ${WEB_OWNER}"
+echo
+
+
+
 echo '***** Installing ES **********'
 install -m 755 -o "${WEB_OWNER}" zmeventnotification.pl "${TARGET_BIN}"
+echo "Done"
 
-echo '***** Installing Hooks **********'
-cd hook
-install -m 755 -o "${WEB_OWNER}" detect_wrapper.sh "${TARGET_BIN}"
-install -m 755 -o "${WEB_OWNER}" detect_yolo.py "${TARGET_BIN}"
-install -m 755 -o "${WEB_OWNER}" detect_hog.py "${TARGET_BIN}"
-python setup.py install 
-cd ..
+read -p "Install machine learning hooks? [Y/n]" confirm
+
+if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]
+then
+    echo '***** Installing Hooks **********'
+    cd hook
+    install -m 755 -o "${WEB_OWNER}" detect_wrapper.sh "${TARGET_BIN}"
+    install -m 755 -o "${WEB_OWNER}" detect_yolo.py "${TARGET_BIN}"
+    install -m 755 -o "${WEB_OWNER}" detect_hog.py "${TARGET_BIN}"
+    python setup.py install 
+    echo "Done"
+    cd ..
+fi
 
 echo
-read -p "Replace zmes and object config files? [y/N]?:" confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+read -p "Replace zmes and object config files? [y/N]?:" confirm 
 
 
-echo '***** Replacing config files *****'
-install ${MAKE_CONFIG_BACKUP} -o "${WEB_OWNER}" -m 644 hook/objectconfig.ini "${TARGET_HOOK_CONFIG}"
-install ${MAKE_CONFIG_BACKUP} -o "${WEB_OWNER}"  -m 644 zmeventnotification.ini "${TARGET_ZMES_CONFIG}"
+if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]
+then
+    echo '***** Replacing config files *****'
+    install ${MAKE_CONFIG_BACKUP} -o "${WEB_OWNER}" -m 644 hook/objectconfig.ini "${TARGET_HOOK_CONFIG}"
+    install ${MAKE_CONFIG_BACKUP} -o "${WEB_OWNER}"  -m 644 zmeventnotification.ini "${TARGET_ZMES_CONFIG}"
+    echo "Done"
+fi
 
-cd ..
