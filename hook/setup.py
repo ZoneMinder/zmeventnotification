86ff67cbc4a2bc6ff6b38d2df74ef74757a5e81d
Pliable Pixels <pliablepixels@gmail.com>
2019-03-06 10:33:23 -0500

    single source versioning

--- hook/setup.py
+++ hook/setup.py
@@ -2,10 +2,10 @@
 
 import io
 import os
-import sys
+import re
+import codecs
 
 from setuptools import setup
-
 
 #Package meta-data.
 NAME = 'zmes_hooks'
@@ -13,10 +13,21 @@ DESCRIPTION = 'ZoneMinder EventServer hooks'
 URL = 'https://github.com/pliablepixels/zmeventserver/tree/master/hook'
 EMAIL = 'pliablepixels@gmail.com'
 AUTHOR = 'Pliable Pixels'
-VERSION = '3.0'
+
+here = os.path.abspath(os.path.dirname(__file__))
+def read(*parts):
+    with codecs.open(os.path.join(here, *parts), 'r') as fp:
+        return fp.read()
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
 
 setup(name = NAME,
-      version=VERSION,
+      version = find_version('zmes_hook_helpers','__init__.py'),
       py_modules = ['zmes_hook_helpers.common_params',
                     'zmes_hook_helpers.log',
                     'zmes_hook_helpers.yolo',
---
+++ hook/zmes_hook_helpers/__init__.py
@@ -0,0 +1,2 @@
+__version__ = "3.2.0"
+VERSION=__version__
