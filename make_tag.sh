e0d86664086049521aa441a5b6e48af6f5aaf9f4
Pliable Pixels <pliablepixels@gmail.com>
2019-11-06 16:18:51 -0500

    new files

---
+++ make_tag.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+if [ -z "$1" ]; then
+    echo "Inferring version name from hooks/zmes_hook_helpers/__init__.py"
+    if [[ `cat hook/zmes_hook_helpers/__init__.py` =~ ^__version__\ =\ \"(.*)\" ]];
+    then
+	    TAGVER=${BASH_REMATCH[1]}
+    else
+	    echo "Bad version parsing"
+	    exit
+    fi
+else
+	TAGVER=$1
+fi
+VER="${TAGVER/v/}"
+echo "Creating tag:v$VER"
+
+read -p "Please generate CHANGELOG and commit it BEFORE you tag. Press a key when ready..."
+read -p "Press any key to create the tag or Ctrl-C to break..." -n1 
+git tag -fa v$VER -m"v$VER"
+git push -f --tags
