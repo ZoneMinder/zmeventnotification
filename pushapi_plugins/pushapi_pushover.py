345dce8fec9c073853e1e50e87f3bad426e1db5d
Pliable Pixels <pliablepixels@gmail.com>
2020-02-28 14:06:48 -0500

    #225 integrate with secrets

--- pushapi_plugins/pushapi_pushover.py
+++ pushapi_plugins/pushapi_pushover.py
@@ -18,21 +18,21 @@
 # MODIFY THESE
 # ===============================================================
 
+
+
 # Look at https://pushover.net/api and put anything you want here
 # just don't add image, title and message as it gets automatically
 # populated later
 
 param_dict = {
-
-    'token': 'YOUR PUSHOVER APP TOKEN',
-    'user': 'YOUR PUSHOVER USER KEY',
+    'token': None, # Leave it as None to read from secrets or put a string value here
+    'user' : None, # Leave it as None to read from secrets or put a string value here
     #'sound':'tugboat',
     #'priority': 0,
     # 'device': 'a specific device',
     # 'url':  'http://whateeveryouwant',
     # 'url_title': 'My URL title',
-
-    
+   
 }
 
 
@@ -46,6 +46,7 @@ import requests
 import pyzm.ZMLog as zmlog
 import os
 
+
 # ES passes the image path, this routine figures out which image
 # to use inside that path
 def get_image(path, cause):
@@ -57,11 +58,18 @@ def get_image(path, cause):
     else:
         return path+'/snapshot.jpg'
 
+# Simple function to read variables from secret file
+def read_secrets(config='/etc/zm/secrets.ini'):
+    from configparser import ConfigParser
+    secrets_object = ConfigParser(interpolation=None)
+    secrets_object.optionxform=str
+    zmlog.Debug(1,'Reading secrets from {}'.format(config))
+    with open(config) as f:
+        secrets_object.read_file(f)
+    return secrets_object._sections['secrets']
 
-# -------- main 
-
+# -------- MAIN ---------------
 zmlog.init(name='zmeventnotification_pushapi')
-
 if len(sys.argv) < 6:
     zmlog.Error ('Missing arguments, got {} arguments, was expecting at least 6: {}'.format(len(sys.argv)-1, sys.argv))
     zmlog.close()
@@ -84,14 +92,24 @@ if len(sys.argv) == 7:
     }
 
 
+if not param_dict['token'] or param_dict['user']:
+    # read from secrets
+    secrets = read_secrets()
+    print (secrets)
+    if not param_dict['token']:
+        param_dict['token'] = secrets.get('PUSHOVER_APP_TOKEN')
+        print (param_dict['token'])
+        zmlog.Debug(1, "Reading token from secrets")
+    if not param_dict['user']:
+        param_dict['user'] = secrets.get('PUSHOVER_USER_KEY'),
+        zmlog.Debug(1, "Reading user from secrets")
+
 param_dict['title'] = '{} Alarm ({})'.format(mname,eid)
 param_dict['message'] = cause +  datetime.now().strftime('at %I:%M %p, %b-%d')
 if event_type == 'event_end':
     param_dict['title'] = 'Ended:' + param_dict['title']
-
 r = requests.post("https://api.pushover.net/1/messages.json", data = param_dict, files = files)
 zmlog.Debug(1,"Pushover returned:{}".format(r.text))
 print(r.text)
-
-
 zmlog.close()
+
