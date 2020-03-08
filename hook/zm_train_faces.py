71a6a3efce71548f8f1c0ab1aaff0cb96ecc03c0
Pliable Pixels <pliablepixels@gmail.com>
2020-03-08 10:02:48 -0400

    fixes https://github.com/pliablepixels/zmeventnotification/issues/232

--- hook/zm_train_faces.py
+++ hook/zm_train_faces.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-import zmes_hook_helpers.face_train as train
 import argparse
 import ssl
 import zmes_hook_helpers.log as log
@@ -9,6 +8,8 @@ import zmes_hook_helpers.utils as utils
 if __name__ == "__main__":
     log.init(process_name='zm_train_faces', dump_console=True)
 # needs to be after log init
+
+import zmes_hook_helpers.face_train as train
 
 if __name__ == "__main__":
     g.ctx = ssl.create_default_context()
