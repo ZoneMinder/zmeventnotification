6a4e1b10ebd9723749f3ea7dc7efb6f41c14a7f6
Pliable Pixels <pliablepixels@gmail.com>
2020-08-09 07:04:56 -0400

    doc

--- install.sh
+++ install.sh
@@ -365,14 +365,16 @@ install_hook() {
 
 # move ES config files
 install_es_config() {
-    echo 'Replacing ES config file'
+    echo 'Replacing ES config & rules file'
     install ${MAKE_CONFIG_BACKUP} -o "${WEB_OWNER}" -g "${WEB_GROUP}"  -m 644 zmeventnotification.ini "${TARGET_CONFIG}" && 
         print_success "config copied" || print_error "could not copy config"
     if [ ! -f "${TARGET_CONFIG}/secrets.ini" ]; then
      install ${MAKE_CONFIG_BACKUP} -o "${WEB_OWNER}" -g "${WEB_GROUP}"  -m 644 secrets.ini "${TARGET_CONFIG}" && 
         print_success "secrets copied" || print_error "could not copy secrets"
     fi
-   
+    echo 'Replacing ES rules file'
+    install ${MAKE_CONFIG_BACKUP} -o "${WEB_OWNER}" -g "${WEB_GROUP}"  -m 644 es_rules.json "${TARGET_CONFIG}" && 
+        print_success "rules copied" || print_error "could not copy rules"
 
 
     echo "====> Remember to fill in the right values in the config files, or your system won't work! <============="
