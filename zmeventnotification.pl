3086e5bc6ec242f2cb553205f2fe8a164bf6037e
Pliable Pixels <pliablepixels@gmail.com>
2020-10-09 07:25:14 -0400

    undefined monlist means all monitors

--- zmeventnotification.pl
+++ zmeventnotification.pl
@@ -2982,7 +2982,7 @@ sub isInList {
   my $monlist = shift;
   my $mid     = shift;
 
-  return 1 if ($monlist eq "");
+  return 1 if ($monlist eq "" || !$monlist);
 
   my @mids = split( ',', $monlist );
   my $found = 0;
