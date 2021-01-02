39bb6cc0f691f6b349e7edf058f65d2b549ae643
Pliable Pixels <pliablepixels@gmail.com>
2021-01-02 11:43:51 -0500

    ver and face training fix

--- zmeventnotification.pl
+++ zmeventnotification.pl
@@ -43,7 +43,7 @@ use Symbol qw(qualify_to_ref);
 use IO::Select;
 
 ####################################
-my $app_version = '6.1.0';
+my $app_version = '6.1.1';
 ####################################
 
 # do this before any log init etc.
