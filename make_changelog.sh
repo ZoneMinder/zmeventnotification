3f347c59a4f56e4791bf12f35a9bbfe7d1010678
Pliable Pixels <pliablepixels@gmail.com>
2021-01-26 11:42:43 -0500

    fix

--- make_changelog.sh
+++ make_changelog.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 if [ -z "$1" ]; then
-    TAGVER=`./zmeventnotification.pl --version`
+    #echo "Getting version name from zm_detect.py"
+    echo 'Getting version from zmeventnotification.pl'
+    if [[ `cat ./zmeventnotification.pl` =~ my\ \$app_version\ =\ \'([0-9]*\.[0-9]*\.[0-9]*)\'\; ]];
+    then
+	    TAGVER=${BASH_REMATCH[1]}
+    else
+	    echo "Bad version parsing"
+	    exit
+    fi
 else
-        TAGVER=$1
+	TAGVER=$1
 fi
 VER="${TAGVER/v/}"
 read -p "Future release is v${VER}. Please press any key to confirm..."
