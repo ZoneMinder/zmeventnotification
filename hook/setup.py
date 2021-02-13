edcd51bb9fe01ae103188a2e009682ce89fab5e5
Pliable Pixels <pliablepixels@gmail.com>
2021-02-13 06:08:42 -0500

    ver

--- hook/setup.py
+++ hook/setup.py
@@ -16,7 +16,7 @@ AUTHOR = 'Pliable Pixels'
 LICENSE = 'GPL'
 INSTALL_REQUIRES = [
     'numpy', 'requests', 'Shapely', 'imutils', 
-    'pyzm>=0.3.33', 'scikit-learn', 'future', 'imageio',
+    'pyzm>=0.3.34', 'scikit-learn', 'future', 'imageio',
     'imageio-ffmpeg','pygifsicle', 'Pillow'
 ]
 
