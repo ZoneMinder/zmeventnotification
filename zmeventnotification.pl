6ae313a5a4912f5dcced5572af9f93c606e5e28c
Pliable Pixels <pliablepixels@gmail.com>
2021-04-03 10:04:30 -0400

    ver

--- zmeventnotification.pl
+++ zmeventnotification.pl
@@ -54,7 +54,7 @@ $ENV{SHELL} = '/bin/sh' if exists $ENV{SHELL};
 delete @ENV{qw(IFS CDPATH ENV BASH_ENV)};
 
 ####################################
-my $app_version = '6.1.18';
+my $app_version = '6.1.19';
 ####################################
 
 # do this before any log init etc.
