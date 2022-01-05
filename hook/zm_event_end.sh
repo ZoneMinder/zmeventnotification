834fa796c4f5022f728e223ea1b31d7e43a1f873
baudneo <baudneo@protonmail.com>
2022-01-04 19:34:41 -0700

    Change hook start and end wrappers to use getopts. Perl daemon code reflects using getopts formatted options of a single dash and character (-dDxrt) with or without a parameter.

--- hook/zm_event_end.sh
+++ hook/zm_event_end.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 trap 'cleanup' SIGINT SIGTERM
-
 # Handle situation of ZM terminates while this is running
 # so notifications are not sent
 cleanup() {
@@ -9,53 +8,72 @@ cleanup() {
    exit 1
 }
 
-# When invoked by zmeventnotification.pl it will be passed:
-# $1 = event Id that triggered an alarm
-# $2 = monitor ID of monitor that triggered an alarm
-# $3 = monitor Name of monitor that triggered an alarm
-# $4 = cause of alarm
-# $5 = 'live' - means a LIVE event for logic
-# $6 = path to event store (if store_frame_in_zm is 1)
 
+# change these to the path of the object detection config file and the path to where ZMES is installed
+config_file="/etc/zm/objectconfig.yml"
+zmes_dir="/var/lib/zmeventnotification"
+# ENVIRONMENT VARIABLES USED BY NEO ZMES (WIP)
+[ -n "$ZMES_CONFIG_FILE" ] && config_file="$ZMES_CONFIG_FILE"
+[ -n "$ZMES_INSTALL_DIR" ] && zmes_dir="$ZMES_INSTALL_DIR"
 
+docker=''
+live=''
+debug=''
+bare_debug=''
+eventpath=''
+mid=''
+eid=''
+name=''
+reason=''
+print_usage() {
+    echo "Usage: $0 [options]"
+    echo "Non-Positional Options (-dDblh):"
+    echo "  -h          Print this help"
 
-# Only tested with ZM 1.32.3+. May or may not work with older versions
-# Logic:
-# This script is invoked by zmeventnotification if you've specified its location in the hook= variable of zmeventnotification.ini
+    echo "  -d          Debug mode (Console output)"
+    echo "  -b          \"Bare\" debug mode (No console output)"
+    echo "  -l          Live mode (From Perl daemon)"
+    echo "  -D          Run in docker mode"
+    echo ""
+    echo "Positional Options (-m 2 -e 321123 -n \"Back Yard\" -r Motion):"
+    echo "  -m          Monitor ID"
+    echo "  -e          Event ID"
+    echo "  -n          Monitor name"
+    echo "  -r          Event reason"
+    echo "  -p          Path to event on disk"
+    echo "  -c          Path to config file (default: $config_file)"
+    echo "  -z          Path to ZMES Install dir (default: $zmes_dir)"
+}
 
+while getopts ":Dlhdbp:m:e:n:r:c:y:" opt
+do
+  case "$opt" in
+    "h") print_usage; exit 0 ;;
+    "D") docker='--docker' ;;
+    "l") live='--live' ;;
+    "d") debug='--debug' ;;
+    "b") bare_debug='--baredebug' ;;
+    "c") config_file=$OPTARG ;;
+    "p") eventpath="$OPTARG" ;;
+    "m") mid="$OPTARG" ;;
+    "e") eid="$OPTARG" ;;
+    "n") name="$OPTARG" ;;
+    "r") reason="$OPTARG" ;;
+    "y") zmes_dir="$OPTARG" ;;
 
-# change this to the path of the object detection config"
-CONFIG_FILE="/etc/zm/objectconfig.yml"
-ZMES_DIR="/var/lib/zmeventnotification"
-LIVE=''
-if [[ "$5" == 'live' ]]; then
-  LIVE='--live'
+    "?") print_usage >&2; exit 1 ;;
+  esac
+done
+
+if [[ -z "$mid" ]]; then
+      DETECTION_SCRIPT=("${zmes_dir}/bin/zm_detect.py" --eventid "$eid" --config "${config_file}"  --reason "${reason}"  --eventpath "${eventpath}" "$docker" "$bare_debug" "$debug" "$live")
+elif [[ -n "$mid" ]]; then
+       DETECTION_SCRIPT=("${zmes_dir}/bin/zm_detect.py" --monitor-id "$mid" --eventid "$eid" --config "${config_file}"  --reason "${reason}"  --eventpath "${eventpath}" "$docker" "$bare_debug" "$debug" "$live")
 fi
-EVENT_PATH="$6"
-REASON="$4"
-
-# Pass the monitor ID so the api creation can be created in its own Thread - if no monitor ID is passed the system will
-# create the api object, login and ask for the monitor ID based on the event ID (adds an extra 1-2 seconds to the
-# response time) PASSING THE MONITOR ID SPEEDS THINGS UP!  SINCE MONITOR id AND --LIVE ARE PASSED WE CAN BE SURE THAT
-# THE MONITOR ID HAS BEEN VERIFIED BEFOREHAND
-# use arrays instead of strings to avoid quote hell
-#if [[ -n "${2}" ]]
-#then
-#  echo "2 arguments"
-#   echo "1 = $1, 2 = $2, 3 = $3, 4 = $4, 5 = $5, 6= $6"
-#   DETECTION_SCRIPT=("${ZMES_DIR}/bin/zm_detect.py" --monitor-id "$2" --event-id "$1" --config "${CONFIG_FILE}" --eventpath "${EVENT_PATH}" --reason "${REASON}" --event-type "end" "$LIVE")
-#else
-#  echo "1 argument"
-#   echo "1 = $1, 2 = $2, 3 = $3, 4 = $4, 5 = $5, 6= $6"
-#   DETECTION_SCRIPT=("${ZMES_DIR}/bin/zm_detect.py" --event-id "$1" --config "${CONFIG_FILE}" --eventpath "${EVENT_PATH}" --reason "${REASON}" --event-type "end" "$LIVE")
-#fi
-## this is why the python script prints out the detection with 'detected:' in the string somewhere
-#RESULTS=$("${DETECTION_SCRIPT[@]}" | grep "detected:")
-#
-#_RET_VAL=1
-## The script needs  to return a 0 for success ( detected) or 1 for failure (not detected)
-#if [[ -n "${RESULTS}" ]]; then
-#   _RET_VAL=0
-#fi
-#echo "${RESULTS}"
+# this is why the python script prints out the detection with 'detected:' in the string somewhere
+RESULTS=$("${DETECTION_SCRIPT[@]}" | grep "detected:")
+_RET_VAL=1
+# The script needs to return a 0 for success (detected) or 1 for failure (not detected)
+[[ -n "${RESULTS}" ]] && _RET_VAL=0
+echo "${RESULTS}"
 exit "${_RET_VAL}"
